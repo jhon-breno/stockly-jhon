9c41f102bbc84c7cf4bd8816f87640fc817a350d
br0067149533@enel.com <jbmarreiro@ayesa.com>
2024-09-27 22:41:29 -0300

    refactor: add data access layer

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,11 +1,11 @@
 import { PlusIcon } from "lucide-react";
 import { Button } from "../_components/ui/button";
-import { db } from "../_lib/prisma";
 import { DataTable } from "../_components/ui/data-table";
 import { productsTableColumns } from "./_components/table-columns";
+import { getProducts } from "../_data-acess/product/get-product";
 
 const ProductPage = async () => {
-  const products = await db.product.findMany({});
+  const products = await getProducts();
   return (
     <div className="m-8 w-full space-y-8 rounded-lg bg-white p-8">
       <div className="flex w-full items-center justify-between">
